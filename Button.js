17aee255a6303989715249f98b8dbe8d90f84379
Adrien Thiery <adrien.thiery@gmail.com>
2016-05-31 13:56:31 -0400

    Added button to repo

---
+++ Button.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+} from 'react-native';
+
+const colors = {
+  defaultBorderColor: "#3B373C",
+};
+
+const styles = StyleSheet.create({
+    container: {
+        height: 40,
+        borderWidth: 1,
+        borderColor: colors.defaultBorderColor,
+        marginTop: 5,
+        marginBottom: 5,
+        borderRadius: 3,
+    },
+    text: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    textContainer: {
+        padding: 10,
+    },
+});
+
+export default class Button extends Component {
+    static propTypes = {
+        backgroundColor: React.PropTypes.string,
+        children: React.PropTypes.oneOfType([
+            React.PropTypes.arrayOf(React.PropTypes.node),
+            React.PropTypes.node,
+        ]),
+        containerStyle: View.propTypes.style,
+        onPress: React.PropTypes.func.isRequired,
+        text: React.PropTypes.string,
+        textContainerStyle: View.propTypes.style,
+        textStyle: Text.propTypes.style,
+        underlayColor: React.PropTypes.string,
+        uppercase: React.PropTypes.bool,
+    };
+
+    render() {
+        return (
+            <View
+                style={[
+                    styles.container,
+                    this.props.backgroundColor
+                        ? { backgroundColor: this.props.backgroundColor }
+                        : {},
+                    this.props.containerStyle,
+                ]}
+            >
+                <TouchableHighlight
+                    underlayColor={this.props.underlayColor}
+                    onPress={this.props.onPress}
+                >
+                    <View
+                        style={[
+                            styles.textContainer,
+                            this.props.textContainerStyle,
+                        ]}
+                    >
+                        {this.props.text
+                            ? <Text
+                                style={[
+                                    styles.text,
+                                    this.props.textStyle,
+                                ]}
+                            >
+                                {this.props.uppercase
+                                    ? this.props.text.toUpperCase()
+                                    : this.props.text
+                                }
+                            </Text>
+                            : null
+                        }
+                        {this.props.children}
+                    </View>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # react-native-simple-button
 A button that implement most of the HTML attributes and flexes itself (that is not a word)
+
+# Properties
+
+Property Name | Type
+--- | ---
+backgroundColor | React.PropTypes.string,
+children | React.PropTypes.oneOfType([ <br> React.PropTypes.arrayOf(React.PropTypes.node), <br> React.PropTypes.node <br>])
+containerStyle | View.propTypes.style
+onPress | React.PropTypes.func.isRequired
+text | React.PropTypes.string
+textContainerStyle | View.propTypes.style
+textStyle | Text.propTypes.style
+underlayColor | React.PropTypes.string
+uppercase | React.PropTypes.bool
+
+
+# TODO
+ * Add `disabled` property
